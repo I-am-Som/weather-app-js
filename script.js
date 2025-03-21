de96e92a9df2478494679b44e349f10ee7efb556
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-03-21 21:20:30 +0530

    ...

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function getWeatherData(lat, lon, API_key) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`)
-        .then(response => {
+        .then((response) => {
             if (!response.ok) {
                 throw new Error(`Error: ${response.status} - ${response.statusText}`);
             }
             return response.json();
         })
-        .then(data => {
-            console.log("Weather Data:", JSON.stringify(data, null, 2)); // Pretty print
+        .then((data) => {
+            document.getElementById("weatherData").innerHTML = JSON.stringify(data);
         })
-        .catch(error => {
+        .catch((error) => {
             console.error("Error fetching weather data:", error);
         });
 }
